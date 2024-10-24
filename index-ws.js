cf57d0e4e13caa529fd9f3de2d5b5c3af59240b9
UnrealTemplier <jaimore.evseev@gmail.com>
2024-10-24 12:56:30 +0400

    Added sqlite connection.

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,16 @@ app.get('/', (req, res) => {
 httpServer.on('request', app);
 httpServer.listen(PORT, () => { console.log('Server started at port 3000') });
 
+process.on('SIGINT', () => {
+    wss.clients.forEach((client) => {
+        client.close();
+    });
+
+    httpServer.close(() => {
+        shutdownDB();
+    });
+});
+
 /** Begin WebSockets */
 
 const WebSocketServer = require('ws').Server;
@@ -26,6 +36,10 @@ wss.on('connection', function connection(ws) {
         ws.send('Welcome to my server!');
     }
 
+    db.run(`INSERT INTO visitors (count, time)
+            VALUES (${numClients}, datetime('now'))
+        `);
+
     ws.on('close', function close() {
         wss.broadcast(`Current visitors: ${numClients}`);
         console.log('A client has disconnected');
@@ -37,3 +51,31 @@ wss.broadcast = function broadcast(data) {
         client.send(data);
     });
 };
+
+/** End WebSockets */
+
+/** Begin Database */
+
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory:');
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )
+    `);
+});
+
+function getCounts() {
+    db.each('SELECT * FROM visitors', (err, row) => {
+        console.log(row);
+    });
+}
+
+function shutdownDB() {
+    getCounts();
+    console.log('Shutting down db');
+    db.close();
+}
