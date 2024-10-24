711d589f8c634f61dac90bc47bf9a0e8208460b2
UnrealTemplier <jaimore.evseev@gmail.com>
2024-10-24 10:47:06 +0400

    Added websocket server.

--- index-ws.js
+++ index-ws.js
@@ -1,10 +1,39 @@
-const express = require('express');
-const server = require('http').createServer();
+import express from 'express';
+const httpServer = require('http').createServer();
 const app = express();
 
+const PORT = 3000;
+
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
+    res.sendFile('index.html', { root: __dirname });
 });
 
-server.on('request', app);
-server.listen(3000, () => { console.log('Server started at port 3000') });
+httpServer.on('request', app);
+httpServer.listen(PORT, () => { console.log('Server started at port 3000') });
+
+/** Begin WebSockets */
+
+import { Server as WebSocketServer } from 'ws';
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on('connection', function connection(wss) {
+    const numClients = wss.clients.size;
+    console.log(`Clients connected: ${numClients}`);
+
+    wss.broadcast(`Current visitors: ${numClients}`);
+
+    if (ws.readyState === ws.OPEN) {
+        ws.send('Welcome to my server!');
+    }
+
+    ws.on('close', function close() {
+        wss.broadcast(`Current visitors: ${numClients}`);
+        console.log('A client has disconnected');
+    });
+});
+
+wss.broadcast = function broadcast(data) {
+    wss.clients.forEach(function each(client) {
+        client.send(data);
+    });
+};
